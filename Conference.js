44c598c480932e88601b838a4bc7a421895235eb
koenidv <32238636+koenidv@users.noreply.github.com>
2021-01-14 16:54:16 +0100

    Saving conference objects

---
+++ Conference.js
@@ -0,0 +1,15 @@
+/**
+ * Conference object constructor
+ */
+class Conference {
+    constructor(title, notes, platform, link, foundlink, date, starttime, endtime) {
+        this.title = title;
+        this.notes = notes;
+        this.platform = platform;
+        this.link = link;
+        this.foundlink = foundlink;
+        this.date = date;
+        this.starttime = starttime;
+        this.endtime = endtime;
+    }
+}
--- background.js
+++ background.js
@@ -1,17 +1,42 @@
+//import { Conference } from './Conference.js'
+
 // On Install
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
     // Create a context menu
     chrome.contextMenus.create({
         id: "savetext",
         title: "Testeintrag!",
-        contexts: [ "selection" ]
+        contexts: ["selection"]
     })
 })
 
 // On context menu item clicked
-chrome.contextMenus.onClicked.addListener( (info, tab) => {
-    // testing some stuff
-    console.log(info)
-    console.log(tab)
-    alert(info.selectionText)
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+
+    // test saving some values to synced storage
+    // First, get current items
+    chrome.storage.sync.get("conferences", (items) => {
+        var conferences = items.conferences
+        var thisConference = new Conference(info.selectionText, "notes", "platform", "link", "foundlink", 0, 1, 2)
+
+        // If this is the first conference, create an array
+        if (conferences === undefined) {
+            conferences = [thisConference]
+        } else {
+            // Else add this conference to the list
+            conferences.push(thisConference)
+        }
+
+        // .. and save all to synced storage
+        chrome.storage.sync.set(
+            { "conferences": conferences },
+            function () {
+                // Log the new value for testing purposes
+                chrome.storage.sync.get("conferences", (newItems) => {
+                    console.log(newItems.conferences)
+                })
+            }
+        )
+    })
+
 })
